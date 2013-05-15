bbc4020b925a47babace0a62cd2964a7953dc4bc
James Thomas <jamesjoethomas@gmail.com>
2013-05-14 23:05:32 -0400

    Removed bad code from Client.java

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,9 +1,6 @@
 package main;
 
 import java.io.IOException;
-import java.net.Socket;
-
-import javax.swing.JOptionPane;
 
 /**
  * GUI chat client runner.
@@ -14,14 +11,6 @@ public class Client {
      * Start a GUI chat client.
      */
     public static void main(String[] args) throws IOException {
-        String hostname = JOptionPane.showInputDialog(
-                "Hostname/IP of server (include port number after colon at end): ");
-        String[] components = hostname.split(":");
-        int port = 5000;
-        if (components.length > 1) {
-            port = Integer.parseInt(components[1]);
-        }
-        Socket s = new Socket(components[0], port);
-        Client
+
     }
 }
