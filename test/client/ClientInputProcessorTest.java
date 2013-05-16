9ad557701cf9a32618dc8ba05af28c3637354e04
Santhosh Narayan <santhoshnarayan@mit.edu>
2013-05-16 14:38:20 -0400

    basic client tests

--- test/client/ClientInputProcessorTest.java
+++ test/client/ClientInputProcessorTest.java
@@ -4,7 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-import message.servertoclient.SMessageImpls.OnlineUserList;
+import message.servertoclient.SMessageImpls.AvailabilityInfo.Status;
+import message.servertoclient.SMessageImpls.NormalAction.ActionType;
+import message.servertoclient.SMessageImpls.*;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,4 +22,70 @@ public class ClientInputProcessorTest {
         Assert.assertArrayEquals(os.toByteArray(), 
                 "ml/addOnlineUsers/john,james\n".getBytes());
     }
+    @Test
+    public void testReturnId() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new ReturnId(12));
+        Assert.assertArrayEquals(os.toByteArray(), 
+                "ml/newId/12\n".getBytes());
+    }
+    @Test
+    public void testNormalActionAddUser() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new NormalAction(12, "james", ActionType.ADD_USER, Arrays.asList("john", "james"), Arrays.asList("john", "james"), ""));
+        Assert.assertEquals(os.toString(), 
+        		"ml/makeConversationListener/12\ncl/addUsers/12/john,james\ncl/addUsers/12/john,james\n");
+    }
+    @Test
+    public void testNormalActionExitConv() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new NormalAction(12, "james", ActionType.EXIT_CONV, Arrays.asList("john", "james"), Arrays.asList("john", "james"), ""));
+        Assert.assertEquals(os.toString(), 
+        		"ml/makeConversationListener/12\ncl/addUsers/12/john,james\ncl/removeUser/12/james\n");
+    }
+    @Test
+    public void testNormalActionTextMessage() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new NormalAction(12, "james", ActionType.TEXT_MESSAGE, Arrays.asList("john", "james"), Arrays.asList("john", "james"), "qwertyuiopasdfghjklzxcvbnm1234567890[]\\;',./"));
+        Assert.assertEquals(os.toString(), 
+                "ml/makeConversationListener/12\ncl/addUsers/12/john,james\ncl/addMessage/12/james/qwertyuiopasdfghjklzxcvbnm1234567890[]\\;',./\n");
+    }
+    @Test
+    public void testAvailabilityInfo() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new AvailabilityInfo("james", Status.ONLINE));
+        Assert.assertEquals(os.toString(), 
+                "ml/addOnlineUsers/james\n");
+    }
+    @Test
+    public void testBadHandle() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new BadHandle("james"));
+        Assert.assertEquals(os.toString(), 
+                "ml/badHandle/james\n");
+    }
+    @Test
+    public void testHandleClaimed() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        ClientInputProcessor cip = new ClientInputProcessor(ml);
+        cip.visit(new HandleClaimed("james"));
+        System.out.println(os.toString());
+        Assert.assertEquals(os.toString(), 
+                "ml/handleClaimed/james\n");
+    }
 }
+
+
--- test/server/ServerInputProcessorTest.java
+++ test/server/ServerInputProcessorTest.java
@@ -2,10 +2,19 @@ package server;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+
+import message.servertoclient.SMessageImpls.NormalAction;
+import message.servertoclient.SMessageImpls.NormalAction.ActionType;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import client.ClientInputProcessor;
+import client.MainListener;
+import client.MainListenerMock;
 
 import testutil.SocketMock;
 
@@ -22,5 +31,15 @@ public class ServerInputProcessorTest {
             t.join();
         }
         Assert.assertTrue(os.toString().startsWith("claimed: james\nid: "));
+    }
+    
+    @Test
+    public void testNormalActionAddUser() throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        MainListener ml = new MainListenerMock(os);
+        //ClientInputProcessor cip = new ClientInputProcessor(ml);
+        //cip.visit(new NormalAction(12, "james", ActionType.ADD_USER, Arrays.asList("john", "james"), Arrays.asList("john", "james"), ""));
+        Assert.assertEquals(os.toString(), 
+        		"ml/makeConversationListener/12\ncl/addUsers/12/john,james\ncl/addUsers/12/john,james\n");
     }
 }
